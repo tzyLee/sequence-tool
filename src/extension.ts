d5d86dc9ef6be9542c18be9ce5eac82d83ef6038
tzyLee <b06901145@ntu.edu.tw>
2023-07-30 22:06:08 +0800

    Implement formatting

--- src/extension.ts
+++ src/extension.ts
@@ -5,9 +5,12 @@ import * as vscode from "vscode";
 type EditOption = { undoStopAfter: boolean, undoStopBefore: boolean }
 type SequenceGen = Iterator<number, number, number>;
 type StepFunction = (prev: number, index: number) => any;
+type Formatter = (n: number) => string;
 interface SequenceGenConstructor {
 	new(init: number, stepFunc: StepFunction): SequenceGen;
 }
+
+const formatPrompt = 'format: [[fillChar][align][width][.prec][spec] ","][init]["," [expr]]'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -25,29 +28,29 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 
 			let editMade = false;
-			const initialLengths = [...editor.selections].sort(sortSelection).map(s => length(s));
+			const initialSelections = [...editor.selections].sort(sortSelection);
 			const sequenceSpec = await vscode.window.showInputBox({
-				title: 'Enter sequence command',
-				placeHolder: 'format: [initialValue][,[(p: prev, i: index) expr]',
+				// title: 'Enter sequence command',
+				placeHolder: formatPrompt,
 				async validateInput(value) {
-					let [gen, msg] = getGenerator(value);
+					let [gen, fmt, msg] = parseCommand(value);
 					if (!gen) {
 						// clear previews when gen == null
 						gen = new IndexSequenceGen(0, () => '');
 					}
 					// When the inputbox is active, the previews cannot be clear with 'undo' command
 					// the text in the inputbox would be undoed instead
-					await insertSequence(editor, initialLengths, gen, { undoStopBefore: !editMade, undoStopAfter: false }, true, editMade);
+					await insertSequence(editor, initialSelections, gen, fmt, { undoStopBefore: !editMade, undoStopAfter: false }, true, editMade);
 					editMade = true;
 					return msg;
 				}
 			});
 
 			if (sequenceSpec) {
-				const [gen, _] = getGenerator(sequenceSpec);
+				const [gen, fmt, _] = parseCommand(sequenceSpec);
 				if (gen) {
 					await vscode.commands.executeCommand('undo')
-					await insertSequence(editor, initialLengths, gen, {
+					await insertSequence(editor, initialSelections, gen, fmt, {
 						undoStopBefore: false,
 						undoStopAfter: true
 					}, false);
@@ -65,33 +68,106 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() { }
 
 
-function getGenerator(v: string): [SequenceGen | null, vscode.InputBoxValidationMessage | string] {
+function parseCommand(v: string): [SequenceGen | null, Formatter, vscode.InputBoxValidationMessage | string] {
+	let formatter = (n: number) => n.toString();
 	if (!v) {
 		// initial empty input won't reach here,
 		// clear the previews after input box is empty
-		return [null, { message: 'Input is empty', severity: vscode.InputBoxValidationSeverity.Info }];
+		return [null, formatter, { message: 'Input is empty', severity: vscode.InputBoxValidationSeverity.Info }];
 	}
-	const matchResult = v.match(/^([+-]?(?:\d+|\d*\.\d+|\d+\.\d*)(?:[Ee][+-]?\d+)?)(?:,(.*))?$/);
+	const match = (/^(?:(?:(?:(?<fillChar>(?!\.)[\D0])(?<align>[<>])?)?(?<width>[1-9]\d*))?(?:\.(?<precision>\d+))?(?:(?:(?<spec>[bodhxfc])|(?:b(?<base>\d+))))?,)?(?:(?<init>[+-]?(?:\d+|\d*\.\d+|\d+\.\d*)(?:[Ee][+-]?\d+)?)?(?:,(?<expr>.+))?)?$/).exec(v);
 	let init = 0;
 	let stepFunc = (p: number, i: number) => p + 1;
 	let Constructor: SequenceGenConstructor = PrevSequenceGen;
+	if (!match) {
+		return [new Constructor(init, stepFunc), formatter, { message: `The command does not match the ${formatPrompt}`, severity: vscode.InputBoxValidationSeverity.Warning }]
+	}
+	let formatCmd = 'default'
+	let initCmd = '0'
+	let exprCmd = '(p, i) => p+1'
+	// groups is not null if there exists any named group
+	const groups = match.groups!;
+	// base conversion: only supports integer
+	if (groups.spec != 'c' && groups.spec != 'f' && groups.spec) {
+		let base = 10;
+		const gbase = parseInt(groups.base)
+		switch (groups.spec) {
+			case 'b': base = 2; break;
+			case 'o': base = 8; break;
+			case 'd': base = 10; break;
+			case 'x': case 'h': base = 16; break;
+			default: if (gbase) {
+				base = gbase;
+			}
+		}
+		formatter = (n: number) => Math.trunc(n).toString(base)
+		formatCmd = `base=${base}`
+	}
+	// precision: only supports decimal floating point
+	if (groups.spec == 'f') {
+		let precision = 6;
+		const gprec = parseInt(groups.precision)
+		if (gprec) {
+			precision = gprec;
+		}
+		formatter = (n: number) => n.toFixed(precision)
+		formatCmd = `precision=${precision}f`
+	}
+	// padding occurs after the number is formatted
+	if (groups.width) {
+		let padLeft = true;
+		let fill = ' ';
+		let width: number | undefined = undefined;
+		if (groups.align == '<') {
+			padLeft = false;
+		}
+		if (groups.width) {
+			width = parseInt(groups.width)
+			fill = groups.fillChar || ' '
+			const prevFormatter = formatter;
+			formatter = (n: number) => padLeft ? prevFormatter(n).padStart(width!!, fill) : prevFormatter(n).padEnd(width!!, fill)
+		}
+		const padCmd = `${fill}${padLeft ? '>' : '<'}${width !== undefined ? width : ''}`
+		formatCmd = formatCmd ? `${padCmd},${formatCmd}` : padCmd
+	}
+	// 2's complement
+	if (groups.spec == 'b' && groups.width && groups.fillChar == '0' && groups.align != '<') {
+		const bitwidth = parseInt(groups.width)
+		const N = 2 ** bitwidth;
+		formatter = (n: number) => {
+			let nint = Math.trunc(n);
+			let fillChar = '0';
+			if (nint < 0) {
+				nint = N + nint;
+				fillChar = '1'
+			}
+			return nint.toString(2).padStart(bitwidth, fillChar)
+		}
+		formatCmd = `0>${groups.width},2's`
+	}
 	try {
-		if (matchResult) {
-			init = parseFloat(matchResult[1]);
-			if (matchResult[2]) {
-				stepFunc = eval(`(function (p,i) { return (${matchResult[2]}); })`)
+		if (groups.expr) {
+			stepFunc = eval(`(function (p,i) { return (${groups.expr}); })`)
+			exprCmd = `(p,i) => ${groups.expr}`
+		}
+		if (groups.init) {
+			if (groups.init) {
+				init = parseFloat(groups.init);
+				initCmd = groups.init
 			}
 		} else {
-			stepFunc = eval(`(function (p,i) { return (${v}); })`)
-			if (!/\bp\b/.test(v)) {
+			if (groups.expr && !/\bp\b/.test(groups.expr)) {
+				// Use index gen if there's no inital value and the expression does not contain p
 				init = stepFunc(0, 0);
+				initCmd = init.toString()
 				Constructor = IndexSequenceGen;
 			}
 		}
 	} catch (e: any) {
-		return [null, { message: e?.message ?? `Invalid function string "${v}"`, severity: vscode.InputBoxValidationSeverity.Warning }]
+		return [null, formatter, { message: `Invalid function string: "${e?.message ?? v}"`, severity: vscode.InputBoxValidationSeverity.Warning }]
 	}
-	return [new Constructor(init, stepFunc), '']
+
+	return [new Constructor(init, stepFunc), formatter, { message: `Command: [${formatCmd}],[${initCmd}],[${exprCmd}]`, severity: vscode.InputBoxValidationSeverity.Info }]
 }
 
 function sortSelection(a: vscode.Selection, b: vscode.Selection): number {
@@ -100,7 +176,7 @@ function sortSelection(a: vscode.Selection, b: vscode.Selection): number {
 	);
 }
 
-async function insertSequence(editor: vscode.TextEditor, initialLengths: number[], gen: SequenceGen, option: EditOption, isPreview: boolean, deleteBeforeInsert: boolean = false) {
+async function insertSequence(editor: vscode.TextEditor, initialSelections: vscode.Selection[], gen: SequenceGen, fmt: Formatter, option: EditOption, isPreview: boolean, deleteBeforeInsert: boolean = false) {
 	return await editor.edit((builder) => {
 		const selections = [...editor.selections].sort(sortSelection);
 		let visibleRange = editor.visibleRanges[0];
@@ -108,24 +184,37 @@ async function insertSequence(editor: vscode.TextEditor, initialLengths: number[
 			editor.visibleRanges.forEach((r) => { visibleRange = visibleRange.union(r) });
 		}
 		// always insert at the end of selections
-		initialLengths.forEach((dChar, i) => {
+		const len = initialSelections.length;
+		for (let i = 0; i < len; i++) {
+			const initSel = initialSelections[i];
+			const sel = selections[i];
 			// Still calculates replacement even when the selection is out of preview range
 			let val = gen.next(i).value;
-			if (isPreview && !visibleRange.contains(selections[i])) {
+			if (isPreview && !visibleRange.contains(sel)) {
+				if (sel.start.isAfter(visibleRange.end)) {
+					// The selections are sorted, so the for loop can stop after encountering the first selection outside the visible range
+					break;
+				}
 				// skip preview of invisible parts
-				return;
+				continue;
 			}
 
 			if (deleteBeforeInsert) {
-				const r = new vscode.Range(selections[i].start.translate(0, +dChar), selections[i].end)
+				let dChar = length(initSel);
+				let r = new vscode.Range(sel.start.translate(0, +dChar), sel.end)
+				if (sel.isEmpty) {
+					// If the selection is empty, it is in cursor mode
+					// The selection is a zero-size selection and moves after insert
+					r = r.with(initSel.end, sel.end)
+				}
 				builder.delete(r)
 			}
 
 			// `delete` + `insert` should be equal to one `replace` call
 			// But when two selections overlap, vscode automatically merges them (which reduces the total number of selections),
 			// so here we separate delete and insert calls instead.
-			builder.insert(selections[i].end, (val === undefined || val === null) ? '' : val.toString())
-		})
+			builder.insert(sel.end, (val === undefined || val === null) ? '' : fmt(val))
+		}
 	}, option)
 }
 
